d4eca82c0f25ad196939849922a7cf5e5ae51e7b
Sean Huvaya <sean.ktxdev@gmail.com>
2023-09-06 15:47:35 +0200

    Initial Commit

---
+++ backend/src/main/java/com/ktxdev/electronix/ElectronixApplication.java
@@ -0,0 +1,13 @@
+package com.ktxdev.electronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronixApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ktxdev/electronix/ElectronixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktxdev.electronix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
